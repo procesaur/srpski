6dae39f2c971acba13404516220855e1e51cee29
Mihailo Škorić <31268468+procesaur@users.noreply.github.com>
2025-02-03 14:12:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(
     version='1.00',
     packages=['srpski'],
     install_requires=[
-        'spacy~=3.0.6',
-        'setuptools~=49.2.1',
-        'requests~=2.23.0',
-        'nltk~=3.4.5'
+        'spacy',
+        'setuptools',
+        'requests'
     ],
     package_data={'srpski': ['sr.abbrev']},
     include_package_data=True,
